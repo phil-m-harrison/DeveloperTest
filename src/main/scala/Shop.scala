bdcc62688170b1c251a6b1d81eea77419f285829
Phil Harrison <philmichaelharrison@gmail.com>
2019-01-15 12:52:50 +0000

    Step 2 function final attempt

--- src/main/scala/Shop.scala
+++ src/main/scala/Shop.scala
@@ -1,12 +1,10 @@
 object Shop extends App{
-  def checkoutManager(shoppingList: List[String])={
-    orangeCost(shoppingList.groupBy(identity).mapValues(_.size)("Orange")) + applesCost(shoppingList.groupBy(identity).mapValues(_.size)("Apple"))
+  def checkoutManager(shoppingList: List[String])= {
+    orangeCost(shoppingList.count(_=="Orange")) + applesCost(shoppingList.count(_=="Apple"))
   }
-  def applesCost(numberOfApples:Int):Double={
-    (numberOfApples/2* 0.6) + (numberOfApples%2 * 0.6)
-  }
-  def orangeCost(numberOfOranges:Int):Double={
-    (numberOfOranges/3 *0.25*2) + (numberOfOranges%3*0.25)
-  }
+  def applesCost(numberOfApples:Int):Double= (numberOfApples/2* 0.6) + (numberOfApples%2 * 0.6)
+
+  def orangeCost(numberOfOranges:Int):Double= (numberOfOranges/3 *0.25*2) + (numberOfOranges%3*0.25)
+
   println(checkoutManager(List("Apple","Apple","Orange","Apple", "Orange", "Orange", "Orange")))
 }
